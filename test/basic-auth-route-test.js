b35b109b1c8555af313e796cc1b8531c277dbeaa
brials <brials008@hotmail.com>
2017-03-13 11:17:06 -0700

    need to pull changes

--- test/basic-auth-route-test.js
+++ test/basic-auth-route-test.js
@@ -9,6 +9,25 @@ const request = require('superagent');
 const User = require('../model/user.js');
 const debug = require('debug')('decor8:basic-auth-route-test');
 
+const serverToggle = require('./lib/server-toggle.js');
+const server = require('../server.js');
+
 mongoose.Promise = Promise;
 
-const url = `http://localhost:${process.env.PORT}`
+const url = `http://localhost:${process.env.PORT}`;
+
+const exampleUser = {
+  username: 'Cayla Zabel',
+  password: 'isacodeboss',
+  email: 'example@email.com'
+};
+
+describe('User Tests', function(){
+  before( done => {
+    serverToggle.serverOn(server, done);
+  });
+  after( done => {
+    serverToggle.serverOff(server, done);
+  });
+  
+})
