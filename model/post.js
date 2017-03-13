de8e6fd7316eb477cb0a874cda0ce151f25be3cb
Jeremiah <jeremiaht.walters@gmail.com>
2017-03-13 14:47:28 -0700

    post model almost complete

--- model/post.js
+++ model/post.js
@@ -1,29 +1,42 @@
 'use strict';
 
-'use strict';
-
+const createError = require('http-errors');
+const Promise = require('bluebird');
+const debug = require('debug')('decor8:post');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const Comment = require('./comment.js');
 //Schema
 const postSchema = Schema({
   name: { type: String, required: true },
   desc: { type: String, required: true },
   userID: { type: Schema.Types.ObjectId, required: true },
   categoryID: { type: Schema.Types.ObjectId, required: true },
-  commentList: [listSchema],
+  comments: [{ type: Schema.Types.ObjectId, ref: 'comment' }],
   imageURI: { type: String, required: true, unique: true },
   objectKey: { type: String, required: true, unique: true },
   created: { type: Date, default: Date.now }
 });
 
-const listSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  postID: {type: Schema.Types.ObjectId, required: true},
-  created: {type: Date, default:Date.now}
-});
-  
+const Post = module.exports = mongoose.model('post', postSchema);
 
-module.exports = mongoose.model('post', postSchema);
-module.exports = mongoose.model('', listSchema);
+Post.findByIdAndAddComment = function(id, comment) {
+  debug('findByIdAndAddComment');
+
+  return Post.findById(id)
+  .catch( err => Promise.reject(createError(404, err.message)))
+  .then( post => {
+    comment.postID = post._id;
+    this.tempPost = post;
+    return new Comment(comment).save();
+  })
+  .then( comment => {
+    this.tempPost.comments.push(comment._id);
+    this.tempComment = comment;
+    return this.tempPost.save();
+  })
+  .then( () => {
+    return this.tempComment;
+  });
+};
