6d400c8c0b34140c6c39b6cef9752a8f8054094d
caylazabel <caylazabel@gmail.com>
2017-03-13 11:46:24 -0700

    user model started

---
+++ model/user.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const createError = require('http-errors');
+const Promise = require('bluebird');
+const debug = require('debug')('decor8:user');
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema ({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  findHash: { type: String, required: true }
+});
+
+userSchema.methods.generatePasswordHash = function(password) {
+  debug('generatePasswordHash');
+
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, 10, (err, hash) => {
+      if(err) return reject(err);
+      this.password = hash;
+      resolve(this);
+    });
+  });
+};
+
+userSchema.methods.comparePasswordHash= function(password) {
+  debug('comparePassword');
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, (err, valid) => {
+      if (err) return reject(err);
+      if(!valid) return reject(createError(401, 'wrong password'));
+      resolve(this);
+    });
+  });
+};
+
+userSchema.methonds.generateFindHash = function() {
+  debug('generateFindHash');
+
+  return new Promise((resolve, reject) => {
+    let attempts = 0;
+
+    _generateFindHash.call(this);
+
+    function _generateFindHash(){
+      this.findHash = crypto.randomBytes(32).toString('hex');
+      this.save()
+      .then( () => resolve(this.findHash))
+      .catch( err => {
+        if(attempts >3) return reject(err);
+        attempts++;
+        _generateFindHash.call(this);
+      });
+    }
+  });
+};
+
+
+userSchema.methods.generateToken = function () {
+  debug('generateToken');
+}
