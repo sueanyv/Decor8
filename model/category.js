5f82d31de00849c0723c58a971950c55524937ca
brials <brials008@hotmail.com>
2017-03-13 16:54:23 -0700

    removing key

--- model/category.js
+++ model/category.js
@@ -5,8 +5,53 @@ const createError = require('http-errors');
 const debug = require('debug')('decor8:category');
 const Schema = mongoose.Schema;
 
+const Post = require('./post.js');
+
 const categorySchema = Schema({
   categoryType: {type:String, required: true},
   created: {type:Date, default: Date.now},
-  desc: {type:String, required:true}
-})
+  desc: {type:String, required:true},
+  postList: [{type: Schema.Types.ObjectId, ref: 'post' }]
+});
+
+const Category = module.exports = mongoose.model('category', categorySchema);
+
+Category.findByIdAndAddPost = function(id, post){
+  debug('findByIdAndAddPost');
+
+  return Category.findById()
+  .catch(err => Promise.reject(createError(404, err.message)))
+  .then(category => {
+    post.categoryID = category._id;
+    this.tempCategory = category;
+    return new Post(post).save();
+  })
+  .then(post => {
+    this.tempCategory.postList.push(post._id);
+    this.tempPost = post;
+    return this.tempCategory.save();
+  })
+  .then(() => {
+    return this.tempPost;
+  });
+};
+
+Category.findByIdAndRemovePost = function(id, postId){
+  debug('findByIdAndAddPost');
+
+  Category.findById(id)
+  .then(category => {
+    for(var i = 0; i < category.postList.length; i++){
+      if(category.postList[i] === postId){
+        category.postList.splice(i, 1);
+      }
+    }
+    Category.findByIdandUpdate(id, category)
+    .then(() => {
+      return;
+    })
+    .catch(err => Promise.reject(createError(404, err.message)));
+    return postId;
+  })
+  .catch(err => Promise.reject(createError(404, err.message)));
+};
--- test/lib/test-env.js
+++ test/lib/test-env.js
@@ -5,5 +5,5 @@ process.env.API_URL='http://localhost:3000';
 process.env.MONGODB_URI='mongodb://localhost/decor8';
 process.env.APP_SECRET='rainydays';
 process.env.AWS_BUCKET='decor8-test';
-process.env.AWS_ACCESS_KEY_ID='AKIAJ3PCJPNCBGQ4Q7UA';
-process.env.AWS_SECRET_ACCESS_KEY='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';
+process.env.AWS_ACCESS_KEY_ID='TESTACCESSKEY';
+process.env.AWS_SECRET_ACCESS_KEY='SECRETKEYTEST';
